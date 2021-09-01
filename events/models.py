3ffcd49262b22c68385245fd895037bd47e80d36
Vinícius Michelutti <viniciusmichelutti@gmail.com>
2021-09-01 12:23:00 -0300

    Event model

--- events/models.py
+++ events/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+from accounts.models import Account
+
+
+class Event(models.Model):
+    type = models.CharField(max_length=255)
+    origin = models.ForeignKey(Account, on_delete=models.DO_NOTHING, null=True, related_name='events_origin')
+    destination = models.ForeignKey(Account, on_delete=models.DO_NOTHING, null=True, related_name='events_destination')
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
