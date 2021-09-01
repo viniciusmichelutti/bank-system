4b705792710cef7ccd1cc13526f678e3d3e4d2b2
Vinícius Michelutti <viniciusmichelutti@gmail.com>
2021-09-01 15:43:52 -0300

    Database migration. Balance field, account model

--- accounts/models.py
+++ accounts/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class Account(models.Model):
     number = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
+    balance = models.DecimalField(max_digits=30, decimal_places=2, default=0)
