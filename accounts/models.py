0bfd871e3886ba56c76b8493e0b3b13f51e5f861
Vinícius Michelutti <viniciusmichelutti@gmail.com>
2021-09-01 12:09:19 -0300

    Account model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class Account(models.Model):
+    number = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
